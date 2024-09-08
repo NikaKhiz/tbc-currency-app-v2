9af7b7ea36216c5fac8de633ca96724b37ff7471
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-08 12:37:12 +0400

    convertation functionality add convertation and clearing functionality

--- converter.py
+++ converter.py
@@ -55,6 +55,13 @@ class CurrencyConverter(QMainWindow):
         # on button click logout user
         self.logoutButton.clicked.connect(self.logout)
 
+        # on button click clear the currency form values
+        self.clearButton.clicked.connect(self.on_click_clear)
+
+        # on button click convert given amount of currency into another and show result
+        self.convertButton.clicked.connect(lambda: self.on_click_convert(self.base_currencies_box.currentText(), self.conversion_currencies_box.currentText()))
+
+
     # load generated ui file by qt designer and center main window 
     def initUI(self):
         uic.loadUi('converter.ui', self)    
@@ -72,20 +79,54 @@ class CurrencyConverter(QMainWindow):
     def fill_up_currencies(self):
         self.base_currencies_box.addItems(currencies_list)
         self.conversion_currencies_box.addItems(currencies_list)
-        self.conversion_currencies_box.setCurrentIndex(1)
+        self.clear_currencies_form()
 
 
     # calculate rate according to given currencies 
     def calculate_current_rate(self, base_currency, conversion_currency):
+        rate = self.convert_currencies(base_currency, conversion_currency)
+        self.rate_label.setText(f'Rate : {rate}')
+
+
+    def convert_currencies(self, base_currency, conversion_currency):
         base_currency_value = CURRENCIES[base_currency]['unit']
         if base_currency != conversion_currency:
             conversion_currency_value = CURRENCIES[base_currency][conversion_currency]
         else:
             conversion_currency_value = base_currency_value
             
-        rate = conversion_currency_value * base_currency_value
-        self.rate_label.setText(f'Rate : {rate}')
+        return conversion_currency_value * base_currency_value
 
+
+    # on button click convert given currency into another and show resutls
+    def on_click_convert(self, base_currency, conversion_currency):
+        try:
+            amount = self.amount_line.text()
+            if not amount:
+                self.result_label.setText('Please choose amount for calculation')
+                return
+
+            amount = float(amount)
+            base_currency = self.base_currencies_box.currentText()
+            conversion_currency = self.conversion_currencies_box.currentText()
+            conversion_rate = self.convert_currencies(base_currency, conversion_currency)
+            converted_amount = amount * conversion_rate
+            self.result_label.setText(f'Result : {int(amount)} {base_currency} is {converted_amount:.2f} {conversion_currency}')
+        except ValueError:
+            self.result_label.setText('Provide valid integer as amount.')
+
+
+    # on button click return form values to initial state   
+    def on_click_clear(self):
+        self.clear_currencies_form()
+        
+
+    def clear_currencies_form(self):
+        self.base_currencies_box.setCurrentIndex(0)
+        self.conversion_currencies_box.setCurrentIndex(1)
+        self.amount_line.setText('')
+        self.calculate_current_rate(self.base_currencies_box.currentText(), self.conversion_currencies_box.currentText())
+        self.result_label.setText('')
 
 
     #  switch from login page to the converter page
